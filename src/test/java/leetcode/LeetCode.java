544f19e2ae6a49845bc7585b504a5eefefe3e329
Hoang.Bui <hoang.bui@doctoranywhere.com>
2022-05-19 13:11:04 +0700

    add UT

---
+++ src/test/java/leetcode/LeetCode.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LeetCode {
+    @Test
+    public void testAdd() {
+        assertEquals(42, Integer.sum(19, 23));
+    }
+}
