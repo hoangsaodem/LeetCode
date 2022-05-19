634e2e26127cde5123e09dfafcc2ccc9ba6fa53d
Hoang.Bui <hoang.bui@doctoranywhere.com>
2022-05-19 11:14:16 +0700

    initial commit

---
+++ src/main/java/leetcode/LeetCode.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LeetCode {
+    public boolean containsDuplicate(int[] nums) {
+        return true;
+    }
+}
---
+++ src/main/java/leetcode/Main.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+
+ 
+
+
+
+    }
+
+
+}
